64b33c86d09927c40091616ef0c4ce44b2400a8d
Luiz Otaviano <77416563+LuizOtaviano@users.noreply.github.com>
2024-02-07 00:26:21 -0300

    adicionando rotas

---
+++ src/http/routes/vote-on-poll.ts
@@ -0,0 +1,65 @@
+import { z } from "zod"
+import { randomUUID } from "node:crypto"
+import { prisma } from "../../lib/prisma"
+import { FastifyInstance } from "fastify"
+
+export async function voteOnPoll(app: FastifyInstance) {
+  app.post("/polls/:pollId/votes", async (req, res) => {
+    const voteOnPollBody = z.object({
+      pollOptionId: z.string().uuid()
+    })
+
+    const voteOnPollParams = z.object({
+      pollId: z.string().uuid()
+    })
+
+    const { pollId } = voteOnPollParams.parse(req.params)
+    const { pollOptionId } = voteOnPollBody.parse(req.body)
+
+    let { sessionId } = req.cookies
+
+    if(sessionId) {
+      const userPreviousVoteOnPoll = await prisma.vote.findUnique({
+        where: {
+          sessionId_pollId: {
+            sessionId,
+            pollId
+          }
+        }
+      })
+
+      if(userPreviousVoteOnPoll && userPreviousVoteOnPoll.pollOptionId !== pollOptionId) {
+        await prisma.vote.delete({
+          where: {
+            id: userPreviousVoteOnPoll.id, 
+          }
+        })
+
+      } else if (userPreviousVoteOnPoll) {
+        return res.status(400).send({ message: 'You already voted on this poll.' })
+      }
+
+    }
+
+    if(!sessionId) {
+      sessionId = randomUUID()
+  
+      res.setCookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        signed: true,
+        httpOnly: true,
+      })
+    }
+
+    await prisma.vote.create({
+      data: {
+        sessionId,
+        pollId,
+        pollOptionId,
+      }
+    })
+
+    return res.status(201).send()
+  })
+}
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,25 +1,18 @@
 import fastify from 'fastify'
-import { z } from 'zod'
-import { PrismaClient } from '@prisma/client'
+import cookie from '@fastify/cookie'
+import { createPoll } from './routes/create-poll'
+import { getPoll } from './routes/get-poll'
+import { voteOnPoll } from './routes/vote-on-poll'
 
 const app = fastify()
-const prisma = new PrismaClient()
 
-app.post('/polls', async (req, res) => {
-  const createPollBody = z.object({
-    title: z.string()
-  })
-
-  const { title } = createPollBody.parse(req.body)
-
-  const poll = await prisma.poll.create({
-    data: {
-      title,
-    }
-  })
-
-  return res.status(201).send({ pollid: poll.id })
+app.register(cookie, {
+  secret: "polls-app-nlw",
+  hook: "onRequest",
 })
+app.register(createPoll)
+app.register(getPoll)
+app.register(voteOnPoll)
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server running!")
